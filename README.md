7b0dc72358debbf33ad73f47908c4f01f7606d63
Dzavid <dzavid.isanovic@gmail.com>
2020-08-06 20:13:59 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# klika-interview
+# Python Assignment - Klika
+
+This repository contains Python script and CSV file which are part of the job application assignment.
+
+The script exports temperature-resistance pairs from thermistor datasheet and exports them to JSON file.
+
+Run script by using 
+`python interview_solution.py -c fileName.csv`
+or 
+`python3 interview_solution.py -c fileName.csv`
+
+where **fileName.csv** is name of your CSV file.
+
+There is an optional "skip lines" parameter (`-s`) which you can use to specify how many header lines of the CSV file should be skipped from the top. I know that sometimes this can be hard for programmers, but counter starts from 1, not 0 :)
+
+In case you want to skip first 100 lines of CSV file, command should be
+`python interview_solution.py -c fileName.csv -s 100`
+
+
+
+**Important note**
+
+There is a pilcrow (¶) symbol in the header of CSV file. Python can't process it (even if you skip header lines). Because of this, Python script will terminate with the following error:
+
+`UnicodeDecodeError: 'charmap' codec can't decode byte 0x83 in position 34: character maps to <undefined>`
+
+Please remove pilcrow symbol and blank space before it in order for script to run without problems.
+
+I have uploaded corrected version of CSV file just in case (**NTCG163JF103FT1-corrected.csv**).
+
+
---
+++ interview_solution.py
@@ -0,0 +1,49 @@
+import csv
+import json
+import argparse
+
+
+def importandexportdata(filename, numofheaderlines=6):
+    with open(filename, mode='r') as csv_file:
+
+        valuesdict = dict()
+        fieldnames = ['Temp', 'Min', 'Nom', 'Max', 'B25/T', '-dT', 'dT']
+        csv_reader = csv.DictReader(csv_file, fieldnames=fieldnames, delimiter=',')
+
+        # Skip given number of header lines
+        for i in range(numofheaderlines):
+            next(csv_reader, None)
+
+        for row in csv_reader:
+
+            if (0 <= int(row["Temp"]) <= 100) and (int(row["Temp"]) % 5 == 0):
+                valuesdict[float(row["Temp"])] = float(row["Nom"])
+
+    # Dump data to json file
+    j = json.dumps(valuesdict, indent=4)
+    f = open(filename.split('.')[0] + '.json', 'w')
+    print(j, file=f)
+    f.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', dest="csvName", required=True, help='(Required) Name of csv file to read from')
+    parser.add_argument('-s', dest="numofheaderlines",
+                        help='(Optional) Number of header lines in csv file (How many lines from the top should be '
+                             'skipped)')
+    args = parser.parse_args()
+
+    numofheaderlines = None
+    numofheaderlines = args.numofheaderlines  # Assign value if provided by user as an argument
+
+    filename = args.csvName
+
+    if (filename.split('.')[1] != 'csv'):
+        print('Given files does not contain csv extension.')
+        exit()
+
+    if numofheaderlines:
+        importandexportdata(filename, int(numofheaderlines))
+    else:
+        importandexportdata(filename)
